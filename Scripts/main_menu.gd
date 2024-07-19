0e5a99ff5ca80cf5967c8d165608c84f4a0f32c3
Mateusz Salach <mateuszsalach00@gmail.com>
2024-07-19 19:27:00 +0200

    Add animations to player, add text_level

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,9 +1,9 @@
 extends Control
 
-@export var level1 = preload("res://Scenes/level1.tscn")
+@export var level1 = preload("res://Scenes/test_level.tscn")
 
-func _ready():
-	GameManager.set_menu_size()
+#func _ready():
+	#GameManager.set_menu_size()
 
 func _on_start_pressed():
 	get_tree().change_scene_to_packed(level1)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@export var SPEED = 300
+@export var SPEED = 100
+@onready var anim_sprite = $AnimatedSprite2D
 
 
 func move_player(time):
@@ -8,6 +9,27 @@ func move_player(time):
 	velocity = movement_direction * SPEED
 
 
+func move_anim():
+	var vertical_movement = Input.get_axis("move_left", "move_right")
+
+	if vertical_movement == 1:
+		anim_sprite.play("walking_sides")
+		anim_sprite.flip_h = false
+	elif vertical_movement == -1:
+		anim_sprite.play("walking_sides")
+		anim_sprite.flip_h = true
+	
+	var horizontal_movement = Input.get_axis("move_up", "move_down")
+	if horizontal_movement == 0:
+		return
+	elif horizontal_movement == 1:
+		anim_sprite.play("walking_down")
+	else:
+		anim_sprite.play("walking_up")
+
+
+
 func _physics_process(delta):
 	move_player(delta)
+	move_anim()
 	move_and_slide()
