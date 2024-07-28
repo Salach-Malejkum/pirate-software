383418ac426724a2b4ed6e3f7227d7667562debe
Mateusz Salach <mateuszsalach00@gmail.com>
2024-07-28 09:41:44 +0200

    Add level change to level 3

--- Scripts/level2.gd
+++ Scripts/level2.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+signal level_entered;
 @onready var spawn_pos = $Marker2D.global_position
 
 
 func _ready():
 	GameManager.player.global_position = spawn_pos
 	add_child(GameManager.player)
+	emit_signal("level_entered")
